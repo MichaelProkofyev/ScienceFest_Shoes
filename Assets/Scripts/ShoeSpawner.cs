9c4df114e946b96e34afbbe83c712c90761a1bcc
Michael Prokofyev <mikeprokofyev@gmail.com>
2018-09-20 14:53:51 +0300

    shoes spawning

--- Assets/Scripts/ShoeSpawner.cs
+++ Assets/Scripts/ShoeSpawner.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class ShoeSpawner : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+
+    public BodySourceView bodySourceView;
+    public float cooldown = 1f;
+    public float destroyDelay = 1f;
+    public float xMaxOffset = 2f;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(SpawnShoes());
 	}
+
+    IEnumerator SpawnShoes()
+    {
+        while(true)
+        {
+            Vector3 spawnPosition = new Vector3(transform.position.x + Random.Range(-xMaxOffset, xMaxOffset), transform.position.y, transform.position.z);
+            var newShoe = Instantiate(bodySourceView.shoeOverlayPrefab, spawnPosition, transform.rotation);
+            var selectedShoeMat = bodySourceView.shoeMaterials[Random.Range(0, bodySourceView.shoeMaterials.Length)];
+            newShoe.GetComponent<Renderer>().material = selectedShoeMat;
+            Destroy(newShoe, destroyDelay);
+            yield return new WaitForSeconds(cooldown);
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
